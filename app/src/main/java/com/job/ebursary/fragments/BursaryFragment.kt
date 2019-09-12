7a0560b5855aa518c646cd674610866308297c09
JobGetabu <getabujob@gmail.com>
2019-09-12 13:54:27 +0300

    added module main

--- app/src/main/java/com/job/ebursary/fragments/BursaryFragment.kt
+++ app/src/main/java/com/job/ebursary/fragments/BursaryFragment.kt
@@ -6,7 +6,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.snackbar.Snackbar
 import com.job.ebursary.R
+import com.job.ebursary.commoners.AdapterListNews
+import com.job.ebursary.commoners.DataGenerator
+import kotlinx.android.synthetic.main.fragment_bursary.*
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -19,6 +25,7 @@ private const val ARG_PARAM2 = "param2"
  */
 class BursaryFragment : Fragment() {
 
+    private lateinit var mAdapter: AdapterListNews
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -27,5 +34,31 @@ class BursaryFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_bursary, container, false)
     }
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        initComponent()
+    }
+
+    private fun initComponent() {
+        recyclerView.layoutManager = LinearLayoutManager(activity)
+        recyclerView.setHasFixedSize(true)
+
+        val items = DataGenerator.getNewsData(activity, 10)
+
+        //set data and list adapter
+        mAdapter = AdapterListNews(activity, items, R.layout.item_news_light)
+        recyclerView.setAdapter(mAdapter)
+
+        // on item list clicked
+        mAdapter.setOnItemClickListener { view, obj, _ ->
+            Snackbar.make(
+                activity!!.findViewById(android.R.id.content),
+                "Item " + obj.title + " clicked",
+                Snackbar.LENGTH_SHORT
+            ).show()
+        }
+
+    }
 
 }
